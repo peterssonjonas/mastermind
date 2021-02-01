dd7963284b2a59e8369702cbc76b1a184cc6f18e
Jonas Petersson <jonasbjornpetersson@gmail.com>
2021-02-01 21:50:43 +0100

    Code cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/App/i);
+  expect(textElement).toBeInTheDocument();
 });
